16839bf8753e260b0e3b3861c6eb18568866fb6a
Eliseo Canaviri <eliseocanaviri44@gmail.com>
2023-08-03 09:30:00 -0400

    Todas la carpetas de MCV

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const BASE_URL = "http://localhost/PlantillaMVC2023/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "loginmvc";
+const CHARSET = "charset=utf8";
+?>
